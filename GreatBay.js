c975c09a3c48c26f05b2aa7f30b94082e34774d2
Frank Valdez <fvaldez421@gmail.com>
2018-02-16 14:30:23 -0800

    fixed sql file, added Add item function

--- GreatBay.js
+++ GreatBay.js
@@ -12,8 +12,31 @@ var connection = mysql.createConnection({
   database: "greatbayDB"
 });
 
+var NewIName = input.name;
+var category = input.category;
+var startBid = input.startBid;
+var currBid = input.currBid;
+var highBid = input.highBid;
+
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   createProduct();
 });
+
+function createProduct() {
+  console.log("Adding new item...");
+  connection.query("INSERT INTO auction SET ?",
+    {
+      item: itemName,
+      category: category,
+      starting_bid: startBid,
+      current_bid: currBid,
+      highest_bid: highBid
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " item added!\n");
+    }
+  );
+}
